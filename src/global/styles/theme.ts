8c972f9e16cd31eba19e3c9271b409a4ba1a33dc
MiguelMarcola <miguelmarcola@gmail.com>
2022-01-15 09:50:54 -0300

    initial config

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+    colors: {
+        primary: '#81AA66',
+        secondary: '#999999',
+        shape: '#FFFFFF',
+        text: '#000000',
+        title: '#969CB2',
+        background: '#F9F9F9',
+    },
+
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold'
+    }
+}
---
+++ src/screens/Catalog/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Greeting } from "./styles";
+
+export function Catalog(){
+    return(
+        <Container>
+            <Greeting>Hello World</Greeting>
+        </Container>
+    );
+}
