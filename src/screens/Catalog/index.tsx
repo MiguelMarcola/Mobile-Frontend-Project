bf76419cf9b7710bdb63c2f79abd4c740292d690
MiguelMarcola <miguelmarcola@gmail.com>
2022-01-15 16:26:49 -0300

    refatoring last update

--- src/screens/Catalog/index.tsx
+++ src/screens/Catalog/index.tsx
@@ -11,7 +11,7 @@ export function Catalog(){
     useEffect(() => {
         async function fetchProducts() {
             try {
-                const response = await api.get('https://5d6da1df777f670014036125.mockapi.io/api/v1/product');
+                const response = await api.get('/product');
                 setProducts(response.data);
             } catch (error){
                 console.log(error);
@@ -22,53 +22,6 @@ export function Catalog(){
         fetchProducts()
     }, []);
 
-    //const newProducts = products.map((products) => products.image.replaceAll(".com", ".com.br"))
-
-
-   /* const data: DatalistPropos[] = [
-        {
-            id: "1",
-            url: "http://lorempixel.com.br/640/480/transport",
-            stock: 91260,
-            name: "Sleek Wooden Soapeek Wooden Soapeek Wooden",
-            value: 5
-        },
-        {
-            id: "2",
-            url: "http://lorempixel.com.br/640/480/transport",
-            stock: 91260,
-            name: "Sleek Wooden",
-            value: 5
-        },
-        {
-            id: "3",
-            url: "http://lorempixel.com.br/640/480/transport",
-            stock: 91260,
-            name: "Sleek Wooden Soap",
-            value: 5
-        },
-        {
-            id: "4",
-            url: "http://lorempixel.com.br/640/480/transport",
-            stock: 91260,
-            name: "Sleek Wooden Soap",
-            value: 5
-        },
-        {
-            id: "5",
-            url: "http://lorempixel.com.br/640/480/transport",
-            stock: 91260,
-            name: "Sleek Wooden Soap",
-            value: 5
-        },
-        {
-            id: "6",
-            url: "http://lorempixel.com.br/640/480/transport",
-            stock: 91260,
-            name: "Sleek Wooden Soap",
-            value: 5
-        }
-    ]*/
     return(
         <Container>
             <Menu />
